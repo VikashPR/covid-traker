3a208a755baa9da1a924a1bbfbca2ee68c627829
vikash2806 <vikashrajendran2806@gmail.com>
2021-04-26 10:27:18 +0530

    header,md-icon added

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav class="nav" flat tile>
+    <v-toolbar dense>
+      <v-btn class="" color="red" dark>
+        Covid Tracker
+        <v-icon dark>
+          md_coronavirus
+        </v-icon>
+      </v-btn>
+      <v-spacer></v-spacer>
+
+      <v-icon color="red">md_warning</v-icon>
+    </v-toolbar>
+  </nav>
+</template>
+
+<script>
+  export default {
+    name: "Header",
+
+  }
+</script>
+
+<style scoped>
+</style>
