61a438eebf0797559c9741a440745a966d01d2c9
vikash2806 <vikashrajendran2806@gmail.com>
2021-04-27 10:52:27 +0530

    header edited

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,14 +1,16 @@
 <template>
-  <nav>
-    <v-toolbar  prominent color="red" class='d-flex justify-center' dark>
-      <v-btn class="ma-2" large color="black" dark>
+  <div>
+    <v-card height="120px" dark prominent class="d-flex flex-wrap justify-center align-center px-4" color="red">
+      <v-btn class="ma-2" large color="black" >
         <h1>Covid-19 Tracker</h1> 
         <v-icon dark center>
           md_coronavirus
         </v-icon>
       </v-btn>
-    </v-toolbar>
-  </nav>
+      <v-spacer></v-spacer>
+      <h1>Devoleped by Vikash P R</h1>
+    </v-card>
+  </div>
 </template>
 
 <script>
